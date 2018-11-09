d0132cc8e6fe4e2db4a5338c1c2f1e495fca3e15
Nefas <nyramirez@gmail.com>
2018-11-08 19:26:44 -0600

    adding arrays to restaurant.js

--- app/data/restaurant.js
+++ app/data/restaurant.js
@@ -1,9 +1,11 @@
-[
+const servingTables = [
     {
-        
-
-    },
-    {
-
+        userId:1,
+        name: "Diana Viana",
+        table: 2,
+        phone_number: 800-300-2034,
+        email: "diana@yoohoo.com"
     }
 ]
+
+const waitingList = []
